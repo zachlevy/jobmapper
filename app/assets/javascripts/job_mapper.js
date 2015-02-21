bdf1b3b620fca6411ed247b0ccef90d1dd76632f
Zachary Levy <zacharyalevy@gmail.com>
2015-02-21 14:17:03 -0500

    added map js store finder

---
+++ app/assets/javascripts/job_mapper.js
@@ -0,0 +1,22 @@
+// create the map on the page
+function createLocator (url) {
+  $('#map-container').storeLocator(
+    {
+      'dataType': 'json',
+      'dataLocation': url,
+      'slideMap' : false,
+      'defaultLoc': true,
+      'defaultLat': '43.655088',
+      'defaultLng' : '-79.388349',
+      'lengthUnit' : 'km',
+      'zoomLevel': 9,
+      'storeLimit' : -1,
+    }
+  );
+}
+
+$(function() {
+	console.log("start");
+	// dont forget to update the infowindow-description template for new products
+	createLocator("http://lcbo.sagemixology.com/getlcbo.php?products=372078,372060");
+});
